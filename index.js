1f1da6535a989368825c68c40d6ec04025aac021
adambisa <adambisa611@gmail.com>
2024-11-23 14:05:09 +0100

    stable status

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require("express");
 var cors = require("cors");
 const app = express();
 
-app.use(cors());
+
+
+app.use(cors()); // ak das toto prec bude incident
 
 const PORT = process.env.PORT || 3000;
 
 
 app.get("/", (req, res) => {
+    res.status(200);
     res.send("Hello, World!");
 })
 
 app.get("/test", (req, res) => {
+    // Disable caching
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private')
     res.status(200);
     res.send("test test");
 })
