52c6c4b8446d7117470973611723acc9225f3713
adambisa <adambisa611@gmail.com>
2024-11-23 13:24:02 +0100

    add test endpoint

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
+})
+
+app.get("/test", (req, res) => {
+    res.status(200);
+    res.send("test test");
 })
 
 app.listen(PORT, ()=>{
